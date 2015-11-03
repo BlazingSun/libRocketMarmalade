65055a4a7fa39d34569e3cecabfcf3cd7c62e61c
Jannik Waschkau <jannik@somyeol.com>
2015-11-03 21:07:18 +0100

    updated build script

--- dependencies.py
+++ dependencies.py
@@ -4,4 +4,6 @@ S3E = os.environ['S3E_DIR']
 if __name__ == "__main__": 
 	if(not os.path.exists("./Dependencies/librocket")):
 		os.system("git clone https://github.com/BlazingSun/libRocket.git ./Dependencies/librocket")
+	else:
+		os.system("git pull https://github.com/BlazingSun/libRocket.git ./Dependencies/librocket")
 	os.system("python "+S3E+"/makefile_builder/mkb.py --make --no-ide ./Dependencies/librocket/Build/marmalade/librocket.mkb")
