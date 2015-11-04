b70453f77f340a654b283f054f1c58c54a5c47be
Jannik Waschkau <jannik@somyeol.com>
2015-11-04 16:05:43 +0100

    fix dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
 	if(not os.path.exists("./Dependencies/librocket")):
 		os.system("git clone https://github.com/BlazingSun/libRocket.git ./Dependencies/librocket")
 	else:
-		os.system("git pull https://github.com/BlazingSun/libRocket.git ./Dependencies/librocket")
+		os.system("git -C ./Dependencies/librocket pull")
 	os.system("python "+S3E+"/makefile_builder/mkb.py --make --no-ide ./Dependencies/librocket/Build/marmalade/librocket.mkb")
