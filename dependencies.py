5493b3cd1ef35a86b06fd155c98c0118f7a98721
Jannik Waschkau <jannik@somyeol.com>
2015-11-05 18:48:54 +0100

    fixed dep

--- dependencies.py
+++ dependencies.py
@@ -5,7 +5,7 @@ S3E = os.environ['S3E_DIR']
 if __name__ == "__main__": 
     if(not os.path.exists("./Dependencies/librocket")):
         os.system("git clone https://github.com/BlazingSun/libRocket.git -b marmalade-libs --single-branch ./Dependencies/librocket")
-        os.system(S3E + "/python/python "+S3E+"/makefile_builder/mkb.py --make --no-ide --compiler=gcc ./Dependencies/librocket/Build/marmalade/librocket.mkb")
-    else:
-        if('up-to-date' not in subprocess.check_output("git -C ./Dependencies/librocket pull")):
-            os.system(S3E + "/python/python "+S3E+"/makefile_builder/mkb.py --make --no-ide --compiler=gcc ./Dependencies/librocket/Build/marmalade/librocket.mkb")
+    #    os.system(S3E + "/python/python "+S3E+"/makefile_builder/mkb.py --make --no-ide --compiler=gcc ./Dependencies/librocket/Build/marmalade/librocket.mkb")
+    #else:
+    #    if('up-to-date' not in subprocess.check_output("git -C ./Dependencies/librocket pull")):
+    #        os.system(S3E + "/python/python "+S3E+"/makefile_builder/mkb.py --make --no-ide --compiler=gcc ./Dependencies/librocket/Build/marmalade/librocket.mkb")
