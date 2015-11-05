05f2e4ad92c936b12b65d336b77003ad6244f2a6
Jannik Waschkau <jannik@somyeol.com>
2015-11-05 14:31:34 +0100

    better dependency management

--- dependencies.py
+++ dependencies.py
@@ -1,9 +1,11 @@
 import os
+import subprocess
 
 S3E = os.environ['S3E_DIR']
 if __name__ == "__main__": 
-	if(not os.path.exists("./Dependencies/librocket")):
-		os.system("git clone https://github.com/BlazingSun/libRocket.git ./Dependencies/librocket")
-	else:
-		os.system("git -C ./Dependencies/librocket pull")
-	os.system("python "+S3E+"/makefile_builder/mkb.py --make --no-ide ./Dependencies/librocket/Build/marmalade/librocket.mkb")
+    if(not os.path.exists("./Dependencies/librocket")):
+        os.system("git clone https://github.com/BlazingSun/libRocket.git ./Dependencies/librocket")
+        os.system(S3E + "/python/python "+S3E+"/makefile_builder/mkb.py --make --no-ide ./Dependencies/librocket/Build/marmalade/librocket.mkb")
+    else:
+        if('up-to-date' not in subprocess.check_output("git -C ./Dependencies/librocket pull")):
+            os.system(S3E + "/python/python "+S3E+"/makefile_builder/mkb.py --make --no-ide ./Dependencies/librocket/Build/marmalade/librocket.mkb")
